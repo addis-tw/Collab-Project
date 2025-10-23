863d96d4b9f5df444fae156c8210340c494248c2
Woldehawariat <awoldehawariat@awoldehawariat.local>
2025-10-23 15:07:11 -0400

    added newVar

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
-"use strict";
+'use strict';
 
-const debug = require("debug")("node-angular");
-const http = require("http");
-const app = require("./src/app");
-const terminate = require("./src/terminate");
+const debug = require('debug')('node-angular');
+const http = require('http');
+const app = require('./src/app');
+const terminate = require('./src/terminate');
+
+const newVar = 'This is a new variable';
 
 const normalizePort = (val) => {
   var port = parseInt(val, 10);
@@ -22,17 +24,17 @@ const normalizePort = (val) => {
 };
 
 const onError = (error) => {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -43,14 +45,14 @@ const onError = (error) => {
 const onListening = async () => {
   const addr = server.address();
 
-  const bind = typeof addr === "string" ? "pipe " + port : "port " + port;
+  const bind = typeof addr === 'string' ? 'pipe ' + port : 'port ' + port;
 
-  console.log("Listening on " + bind);
-  debug("Listening on " + bind);
+  console.log('Listening on ' + bind);
+  debug('Listening on ' + bind);
 };
 
 const port = normalizePort(process.env.PORT || process.env.DEV_PORT);
-app.set("port", port);
+app.set('port', port);
 
 const server = http.createServer(app);
 
@@ -59,10 +61,10 @@ const exitHandler = terminate(server, {
   timeout: 500,
 });
 
-process.on("uncaughtException", exitHandler(1, "Unexpected Error"));
-process.on("unhandledRejection", exitHandler(1, "Unhandled Promise"));
-process.on("SIGTERM", exitHandler(0, "SIGTERM"));
-process.on("SIGINT", exitHandler(0, "SIGINT"));
-server.on("error", onError);
-server.on("listening", onListening);
+process.on('uncaughtException', exitHandler(1, 'Unexpected Error'));
+process.on('unhandledRejection', exitHandler(1, 'Unhandled Promise'));
+process.on('SIGTERM', exitHandler(0, 'SIGTERM'));
+process.on('SIGINT', exitHandler(0, 'SIGINT'));
+server.on('error', onError);
+server.on('listening', onListening);
 server.listen(port);
